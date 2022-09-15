7afa7abad3268edc1421011d738dcd58ce35626f
Kamil A <1505kamil@gmail.com>
2022-09-15 10:46:07 +0500

    Update Cinema.java

--- src/Cinema.java
+++ src/Cinema.java
@@ -1,53 +1,31 @@
-
 import java.util.Scanner;
 
 public class Cinema {
-
+    private final Scanner scan = new Scanner(System.in);
+    private char[][] room;
     private int rows;
     private int rowSeats;
     private int allSeats;
 
     public static void main(String[] args) {
         Cinema cinema = new Cinema();
-        cinema.showSeats();
-        cinema.book();
+        cinema.initRoom();
+        cinema.showMenu();
     }
 
     public void showSeats() {
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Enter the number of rows:");
-        rows = scan.nextInt();
-        System.out.println("Enter the number of seats in each row:");
-        rowSeats = scan.nextInt();
-        allSeats = rows * rowSeats;
         System.out.println("\nCinema:");
-        for (int i = 0; i <= rows; i++) {
-            for (int j = 0; j <= rowSeats; j++) {
-                if (i == 0 && j == 0) System.out.print("  ");
-                else if (i == 0) System.out.print(j + " ");
-                else if (j == 0) System.out.print(i + " ");
-                else System.out.print("S ");
+        for (int i = 0; i < room.length; i++) {
+            for (int j = 0; j < room[i].length; j++) {
+                System.out.print(room[i][j] + " ");
             }
             System.out.println();
         }
-    }
-
-    public void showBookedSeat(int n, int m) {
-        System.out.println("\nCinema:");
-        for (int i = 0; i <= rows; i++) {
-            for (int j = 0; j <= rowSeats; j++) {
-                if (i == 0 && j == 0) System.out.print("  ");
-                else if (i == 0) System.out.print(j + " ");
-                else if (j == 0) System.out.print(i + " ");
-                else if (i == n && j == m) System.out.print("B ");
-                else System.out.print("S ");
-            }
-            System.out.println();
-        }
+        System.out.println();
+        showMenu();
     }
 
     public void showTotalIncome() {
-        Scanner scan = new Scanner(System.in);
         System.out.println("Enter the number of rows:");
         rows = scan.nextInt();
         System.out.println("Enter the number of seats in each row:");
@@ -83,14 +61,57 @@ public class Cinema {
         return price;
     }
 
-    public void book() {
-        Scanner scan = new Scanner(System.in);
+    public void buyTicket() {
         System.out.println("\nEnter a row number:");
         int row = scan.nextInt();
         System.out.println("Enter a seat number in that row:");
         int seat = scan.nextInt();
         int price = seatPrice(row, seat);
         System.out.println("Ticket price: $" + price);
-        showBookedSeat(row, seat);
+        room[row][seat] = 'B';
+        showMenu();
+    }
+
+    public void initRoom() {
+        System.out.println("Enter the number of rows:");
+        rows = scan.nextInt();
+        System.out.println("Enter the number of seats in each row:");
+        rowSeats = scan.nextInt();
+        allSeats = rows * rowSeats;
+        room = new char[rows + 1][rowSeats + 1];
+        for (int i = 0; i < rows + 1; i++) {
+            for (int j = 0; j < rowSeats + 1; j++) {
+                if (i == 0 && j == 0) {
+                    room[i][j] = ' ';
+                } else if (i == 0) {
+                    room[i][j] = (char) (j + 48);
+                } else if (j == 0) {
+                    room[i][j] = (char) (i + 48);
+                } else {
+                    room[i][j] = 'S';
+                }
+            }
+        }
+    }
+
+    public void showMenu() {
+        System.out.println("""
+                1. Show the seats
+                2. Buy a ticket
+                0. Exit
+                """);
+        int option = scan.nextInt();
+        switch (option) {
+            case (1):
+                showSeats();
+                break;
+            case (2):
+                buyTicket();
+                break;
+            case (0):
+                break;
+            default:
+                System.out.println("Invalid option");
+        }
     }
 }
