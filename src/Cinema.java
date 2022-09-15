ff6acb1139343d372382786b4092cb4cf13c9bc2
Kamil A <1505kamil@gmail.com>
2022-09-15 09:48:10 +0500

    Update Cinema.java

--- src/Cinema.java
+++ src/Cinema.java
@@ -3,15 +3,26 @@ import java.util.Scanner;
 
 public class Cinema {
 
+    private int rows;
+    private int rowSeats;
+    private int allSeats;
+
     public static void main(String[] args) {
         Cinema cinema = new Cinema();
         cinema.showSeats();
-        cinema.showTotalIncome();
+        cinema.book();
     }
+
     public void showSeats() {
-        System.out.println("Cinema:");
-        for (int i = 0; i < 8; i++) {
-            for (int j = 0; j < 9; j++) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the number of rows:");
+        rows = scan.nextInt();
+        System.out.println("Enter the number of seats in each row:");
+        rowSeats = scan.nextInt();
+        allSeats = rows * rowSeats;
+        System.out.println("\nCinema:");
+        for (int i = 0; i <= rows; i++) {
+            for (int j = 0; j <= rowSeats; j++) {
                 if (i == 0 && j == 0) System.out.print("  ");
                 else if (i == 0) System.out.print(j + " ");
                 else if (j == 0) System.out.print(i + " ");
@@ -20,22 +31,66 @@ public class Cinema {
             System.out.println();
         }
     }
+
+    public void showBookedSeat(int n, int m) {
+        System.out.println("\nCinema:");
+        for (int i = 0; i <= rows; i++) {
+            for (int j = 0; j <= rowSeats; j++) {
+                if (i == 0 && j == 0) System.out.print("  ");
+                else if (i == 0) System.out.print(j + " ");
+                else if (j == 0) System.out.print(i + " ");
+                else if (i == n && j == m) System.out.print("B ");
+                else System.out.print("S ");
+            }
+            System.out.println();
+        }
+    }
+
     public void showTotalIncome() {
         Scanner scan = new Scanner(System.in);
         System.out.println("Enter the number of rows:");
-        int rows = scan.nextInt();
+        rows = scan.nextInt();
         System.out.println("Enter the number of seats in each row:");
-        int rowSeats = scan.nextInt();
-        int allSeats = rows * rowSeats;
-        int income;
+        rowSeats = scan.nextInt();
+        allSeats = rows * rowSeats;
+        int income = 0;
         if (allSeats <= 60) {
             income = allSeats * 10;
         } else {
             int firstHalf = (rows / 2) * rowSeats;
             int secondHalf = allSeats - firstHalf;
-            income =firstHalf * 10 + secondHalf * 8;
+            income = firstHalf * 10 + secondHalf * 8;
         }
         System.out.printf("Total income:%n$%d", income);
         scan.close();
+    }
+
+    public int seatPrice(int n, int m) {
+        int price = 0;
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j <= m; j++) {
+                if (allSeats <= 60) {
+                    price = 10;
+                } else {
+                    if (n <= rows / 2) {
+                        price = 10;
+                    } else {
+                        price = 8;
+                    }
+                }
+            }
+        }
+        return price;
+    }
+
+    public void book() {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("\nEnter a row number:");
+        int row = scan.nextInt();
+        System.out.println("Enter a seat number in that row:");
+        int seat = scan.nextInt();
+        int price = seatPrice(row, seat);
+        System.out.println("Ticket price: $" + price);
+        showBookedSeat(row, seat);
     }
 }
