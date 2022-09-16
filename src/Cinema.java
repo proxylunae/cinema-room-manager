7dc3aace67b59614efc2fa29df335f441aadad8a
Kamil A <1505kamil@gmail.com>
2022-09-16 12:05:10 +0500

    final?

--- src/Cinema.java
+++ src/Cinema.java
@@ -1,23 +1,24 @@
 import java.util.Scanner;
 
 public class Cinema {
-    private final Scanner scan = new Scanner(System.in);
-    private char[][] room;
+    private final Scanner SCANNER = new Scanner(System.in);
+    private String[][] room;
     private int rows;
     private int rowSeats;
     private int allSeats;
+    private int soldTickets = 0;
+    private int currentIncome = 0;
 
     public static void main(String[] args) {
         Cinema cinema = new Cinema();
-        cinema.initRoom();
         cinema.showMenu();
     }
 
     public void showSeats() {
         System.out.println("\nCinema:");
-        for (int i = 0; i < room.length; i++) {
-            for (int j = 0; j < room[i].length; j++) {
-                System.out.print(room[i][j] + " ");
+        for (String[] strings : room) {
+            for (String string : strings) {
+                System.out.print(string + "\t");
             }
             System.out.println();
         }
@@ -25,13 +26,8 @@ public class Cinema {
         showMenu();
     }
 
-    public void showTotalIncome() {
-        System.out.println("Enter the number of rows:");
-        rows = scan.nextInt();
-        System.out.println("Enter the number of seats in each row:");
-        rowSeats = scan.nextInt();
-        allSeats = rows * rowSeats;
-        int income = 0;
+    public int calculateTotalIncome() {
+        int income;
         if (allSeats <= 60) {
             income = allSeats * 10;
         } else {
@@ -39,8 +35,7 @@ public class Cinema {
             int secondHalf = allSeats - firstHalf;
             income = firstHalf * 10 + secondHalf * 8;
         }
-        System.out.printf("Total income:%n$%d", income);
-        scan.close();
+        return income;
     }
 
     public int seatPrice(int n, int m) {
@@ -63,55 +58,104 @@ public class Cinema {
 
     public void buyTicket() {
         System.out.println("\nEnter a row number:");
-        int row = scan.nextInt();
+        int row = SCANNER.nextInt();
         System.out.println("Enter a seat number in that row:");
-        int seat = scan.nextInt();
+        int seat = SCANNER.nextInt();
+        if (row > rows || seat > rowSeats) {
+            System.out.println("\nWrong input!");
+            buyTicket();
+            return;
+        }
+        if (room[row][seat].equals("B")) {
+            System.out.println("That ticket has already been purchased!");
+            buyTicket();
+            return;
+        }
         int price = seatPrice(row, seat);
         System.out.println("Ticket price: $" + price);
-        room[row][seat] = 'B';
+        room[row][seat] = "B";
+        soldTickets++;
+        currentIncome += price;
         showMenu();
     }
 
     public void initRoom() {
         System.out.println("Enter the number of rows:");
-        rows = scan.nextInt();
+        rows = SCANNER.nextInt();
         System.out.println("Enter the number of seats in each row:");
-        rowSeats = scan.nextInt();
+        rowSeats = SCANNER.nextInt();
         allSeats = rows * rowSeats;
-        room = new char[rows + 1][rowSeats + 1];
+        room = new String[rows + 1][rowSeats + 1];
         for (int i = 0; i < rows + 1; i++) {
             for (int j = 0; j < rowSeats + 1; j++) {
                 if (i == 0 && j == 0) {
-                    room[i][j] = ' ';
+                    room[i][j] = " ";
                 } else if (i == 0) {
-                    room[i][j] = (char) (j + 48);
+                    room[i][j] = String.valueOf(j);
                 } else if (j == 0) {
-                    room[i][j] = (char) (i + 48);
+                    room[i][j] = String.valueOf(i);
                 } else {
-                    room[i][j] = 'S';
+                    room[i][j] = "S";
                 }
             }
         }
+        currentIncome = 0;
+        soldTickets = 0;
+        showMenu();
     }
 
     public void showMenu() {
-        System.out.println("""
-                1. Show the seats
+        System.out.print("""
+                \n1. Create new room
                 2. Buy a ticket
+                3. Statistics
+                4. Show the seats
+                5. Cancel ticket
                 0. Exit
                 """);
-        int option = scan.nextInt();
+        int option = SCANNER.nextInt();
         switch (option) {
             case (1):
-                showSeats();
+                initRoom();
                 break;
             case (2):
                 buyTicket();
+                break;
+            case (3):
+                showStats();
+                break;
+            case (4):
+                showSeats();
+                break;
+            case (5):
+                cancelTicket();
                 break;
             case (0):
                 break;
             default:
                 System.out.println("Invalid option");
         }
+    }
+
+    public void showStats() {
+        double percent = (double) soldTickets / ((double) allSeats / 100);
+        System.out.printf("%nNumber of purchased tickets: %d%n" +
+                "Percentage: %.2f%%%n" +
+                "Current income: $%d%n" +
+                "Total income: $%d%n", soldTickets, percent, currentIncome, calculateTotalIncome());
+        showMenu();
+    }
+
+    public void cancelTicket() {
+        System.out.println("Enter a row number:");
+        int row = SCANNER.nextInt();
+        System.out.println("Enter a seat number in that row:");
+        int seat = SCANNER.nextInt();
+        room[row][seat] = "S";
+        int price = seatPrice(row, seat);
+        currentIncome -= price;
+        soldTickets -= 1;
+        System.out.println("Your ticket was canceled successfully");
+        showMenu();
     }
 }
